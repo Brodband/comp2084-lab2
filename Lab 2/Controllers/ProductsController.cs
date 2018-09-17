95355f4c76a05338ecd6acd9663836a7646b5de7
Justyn <200367460@student.georgianc.on.ca>
2018-09-17 18:11:49 -0400

    Initial Commit

---
+++ Lab 2/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        public ActionResult Product(string ProductName)
+        {
+            ViewBag.ProductName = ProductName;
+            return View();
+        }
+    }
+}
